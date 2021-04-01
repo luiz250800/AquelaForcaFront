1423743172bcc89fe8a59d3b22091c44081ebe84
Augusto Luna <caugustolunas@gmail.com>
2021-04-01 12:10:55 -0300

    Feita a tela de cadastro de responsavel

--- projeto-hackaton/src/pages/ParentRegisterPage/ParentRegisterPage.js
+++ projeto-hackaton/src/pages/ParentRegisterPage/ParentRegisterPage.js
@@ -1,10 +1,57 @@
 import React from "react";
+import {
+  MainDiv,
+  TitleDiv,
+  ButtonsDiv,
+  RoleButton,
+  InputsDiv,
+  NextButton,
+  DisclaimerDiv,
+} from "./Styled";
+import { IoMdPerson } from "react-icons/io";
+import { FaGraduationCap } from "react-icons/fa";
 
 const ParentRegisterPage = () => {
   return (
-    <div>
-      <p> Cadastro de responsável </p>
-    </div>
+    <MainDiv>
+      <TitleDiv>
+        <h3> Vamos começar cadastrando algumas informações: </h3>
+      </TitleDiv>
+      <ButtonsDiv>
+        <RoleButton>
+          <IoMdPerson size="20px" /> Responsável
+        </RoleButton>
+        <RoleButton>
+          <FaGraduationCap size="20px" /> Escola
+        </RoleButton>
+      </ButtonsDiv>
+      <InputsDiv>
+        <label>
+          Nome do Responsável
+          <input />
+        </label>
+        <label>
+          E-mail
+          <input />
+        </label>
+        <label>
+          Senha
+          <input />
+        </label>
+      </InputsDiv>
+      <NextButton> Próximo </NextButton>
+      <DisclaimerDiv>
+        <h4> Termos informando que o aluno não será exposto </h4>
+        <p>
+          Etiam scelerisque sodales risus, nec rhoncus nunc dignissim non. Nunc
+          placerat nisl eget justo tincidunt facilisis. Nam et justo mi. Nam
+          pellentesque semper suscipit. Aliquam diam leo, bibendum id diam quis,
+          hendrerit congue dolor. Proin sodales nibh diam, non mattis nulla
+          semper eu. Vestibulum risus est, vulputate ut pellentesque vitae,
+          aliquet at risus. Quisque porttitor enim nec iaculis tempor.
+        </p>
+      </DisclaimerDiv>
+    </MainDiv>
   );
 };
 
